1e49db61375e56a5e6bf5bd224f2207a9e2c90cc
Sabino Velasquez <sabino@Sabinos-MacBook-Pro.local>
2015-08-19 22:58:22 -0300

    what i do section

---
+++ source/js/lib/script.js
@@ -0,0 +1,14 @@
+'use-strict';
+
+
+// $('video').hover(function(){
+// 	$(this).get(0).play();
+// }, function(){
+// 	$(this).get(0).pause();
+// });
+
+
+// $(document).ready(function(){
+// 	$('#vid1').get(0).pause();
+// 	$('#vid2').get(0).pause();
+// })
