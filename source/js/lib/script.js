72e7c85b3ccaf0f123d05e327b3358ed7ef0fca8
Sabino Velasquez <depointless@Sabinos-Mac-mini.local>
2015-08-20 21:32:05 -0300

    fixed img size in "i do" section

--- source/js/lib/script.js
+++ source/js/lib/script.js
@@ -1,8 +1,15 @@
 'use-strict';
 
+function resize(){
+	var h = $('.vid-full img').width();
+	$('.vid-med img').height(h/2);
+}
 
-$('document').ready(function() {
+$(window).resize(resize);
+
+$(document).ready(function() {
 	var options = { videoId: 'Q0iOS_8BsdM', start: 3 };
 	$('#how').tubular(options);
 	$('.parallax').parallax();
+	resize();
 });
