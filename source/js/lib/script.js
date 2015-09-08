d19e48cbf2bb1f3edc9c4b76068ac29cf1bf491f
Sabino Velasquez <sabino@Sabinos-MacBook-Pro.local>
2015-09-07 22:53:16 -0300

    like button

--- source/js/lib/script.js
+++ source/js/lib/script.js
@@ -52,3 +52,11 @@ $(document).ready(function() {
 		// console.log(url);
 	});
 });
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_EN/sdk.js#xfbml=1&version=v2.4&appId=185131048484498";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
