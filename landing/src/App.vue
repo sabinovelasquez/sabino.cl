1b9759ef17c2cd4bd8984b36d72cebeff6fce27a
Sabino Velasquez <sabinovelasquez@gmail.com>
2019-02-12 13:56:19 -0300

    added pug

--- landing/src/App.vue
+++ landing/src/App.vue
@@ -1,8 +1,7 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+<template lang='pug'>
+  #app
+    img(src="./assets/logo.png")
+    router-view
 </template>
 
 <script>
@@ -11,7 +10,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang='scss'>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
